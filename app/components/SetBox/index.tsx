6c0f455f020896607ecdcffe97d7904f5b1eabda
Nico Dann <nicodann@gmail.com>
2024-04-26 09:06:50 -0400

    Make entire SetBox a link.

--- app/components/SetBox/index.tsx
+++ app/components/SetBox/index.tsx
@@ -10,19 +10,19 @@ interface SetBoxProps extends Set {
 
 export default function SetBox(setData: SetBoxProps) {
   return (
-    <Flex direction='column' width={200} height={200} bg='lavender' p={12} align='center' justify='space-around'>
-      <h2>
-        {setData.name}
-      </h2>
-      <Image 
-        src={setData.image_url 
-          ? setData.image_url 
-          : '/images/Lego_uh_oh.png'} alt={`Lego ${setData.number} ${setData.name}`} 
-        // fill={true}
-        width={100}
-        height={100}
-      />
-      <Link href={setData.url}>instructions</Link>
-    </Flex>
+    <Link href={setData.url}>
+      <Flex direction='column' width={200} height={200} bg='lavender' p={12} align='center' justify='space-around'>
+        <h2>
+          {setData.name}
+        </h2>
+        <Image 
+          src={setData.image_url 
+            ? setData.image_url 
+            : '/images/Lego_uh_oh.png'} alt={`Lego ${setData.number} ${setData.name}`} 
+          width={100}
+          height={100}
+        />
+      </Flex>
+    </Link>
   )
 }
